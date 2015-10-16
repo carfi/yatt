352cd4fd94cd111ea8898112d78e6202ee19a89c
Davide Carfi <carfid@gmail.com>
2015-10-16 15:36:57 +0200

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yattApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yattApp
+ */
+angular.module('yattApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yattApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yattApp
+ */
+angular.module('yattApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
